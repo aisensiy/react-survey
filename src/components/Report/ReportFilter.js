6adc95d9cb556b4c6fb9a39147f51fe96d81b0bd
aisensiy <aisensiy@163.com>
2016-09-12 12:47:13 +0800

    label for filter

--- src/components/Report/ReportFilter.js
+++ src/components/Report/ReportFilter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Label } from 'react-bootstrap';
 import QuestionOptionList from './QuestionOptionList';
 import { canReportTypes } from '../../reducers/data';
 
@@ -51,10 +51,38 @@ class ReportFilter extends React.Component {
     )
   }
 
+  filterLabels() {
+    let { survey, reportFilter, hasFilterMap } = this.props;
+    let labels = [];
+    {Object.keys(reportFilter).filter(k => hasFilterMap[k]).forEach(questionId => {
+      let question = survey.questions.find(q => q._id === questionId);
+      question.options.forEach(option => {
+        if (reportFilter[questionId][option._id]) {
+          labels.push({
+            question: question,
+            option: option
+          });
+        }
+      });
+    })}
+    return labels;
+  }
+
   render() {
+    let { updateFilter, reportFilter } = this.props;
+
     return (
         <div>
           <Button bsStyle="primary" bsSize="xsmall" onClick={this.open.bind(this)}>Filter</Button>
+          {this.filterLabels().map(({ question, option }) => {
+            return (
+                <Label bsStyle="info">{question.title} { ' ' } : { ' ' } {option.content}
+                  <span onClick={() => {
+                    updateFilter(reportFilter, {question: question._id, option: option._id, value: false});
+                  }}>x</span>
+                </Label>
+            );
+          })}
           {this.renderModal()}
         </div>
     );
